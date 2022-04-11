dfdc626c88abe47f57108e29b2cb8761b0d86254
guilherme <guilherme.hlg.eng@gmail.com>
2022-04-11 17:43:39 -0300

    inicia desenvolvimento do padrao repository no controller de planos

--- larafood-backend/app/Http/Controllers/Admin/PlansController.php
+++ larafood-backend/app/Http/Controllers/Admin/PlansController.php
@@ -3,12 +3,21 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Plan;
 use Illuminate\Http\Request;
 
 class PlansController extends Controller
 {
+    private $planRepository;
+
+    public function __construct(Plan $plan)
+    {
+        $this->planRepository = $plan;
+    }
     public function index()
     {
+        $plans = $this->repository->all();
+
         return view('admin.pages.plans.index');
     }
 }
